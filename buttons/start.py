985a5ab2973213185afcb1bd14542a8a92dee85f
OneZeroZeroOneOne <intheendreallife@gmail.com>
2019-08-31 18:02:39 +0300

    suka bombit

--- buttons/start.py
+++ buttons/start.py
@@ -5,9 +5,9 @@ from aiogram.utils.callback_data import CallbackData
 from loguru import logger
 
 
-start_cb = CallbackData('skip','start')
+start_cb = CallbackData('skip','new_game')
 
 def start():
     markup = InlineKeyboardMarkup()
-    markup.add(InlineKeyboardButton("НАЧАТЬ", callback_data=start_cb.new(start = 'start')))
+    markup.add(InlineKeyboardButton("НАЧАТЬ", callback_data=start_cb.new(new_game = 'new_game')))
     return markup
--- handlers.py
+++ handlers.py
@@ -1,7 +1,7 @@
 from aiogram.types import ContentType, ChatMember
 import settings
 from aiogram import Bot, Dispatcher, executor, types
-from buttons.start import start
+from buttons import start
 from loguru import logger
 
 class Handlers:
@@ -11,7 +11,7 @@ class Handlers:
 
     def register(self):
         self.dp.register_message_handler(self.start, commands='start')
-        self.dp.register_callback_query_handler(self.query_skip, skip.skip_cb.filter(ng='new_game'))
+        self.dp.register_callback_query_handler(self.new_game, start.start_cb.filter(new_game='new_game'))
 
     async def new_game(self, query: types.CallbackQuery, callback_data: dict):
         await query.bot.send_message(query.message.chat.id,"preiom")
@@ -21,4 +21,4 @@ class Handlers:
     async def start(self, message: types.Message):
         logger.info(message.chat.id)
         await self.bot.send_message(message.chat.id, text=settings.start_text,
-                                    parse_mode = 'Markdown', reply_markup=start())
+                                    parse_mode = 'Markdown', reply_markup=start.start())
